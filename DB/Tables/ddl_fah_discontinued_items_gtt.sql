201e71be55d360c6edddd53c0f3f8f2bd904a09f
fabianekirsten <122301571+fabianekirsten@users.noreply.github.com>
2023-06-01 16:50:38 -0300

    ini

--- DB/Tables/ddl_fah_discontinued_items_gtt.sql
+++ DB/Tables/ddl_fah_discontinued_items_gtt.sql
@@ -3,29 +3,12 @@
 * Change History:
 * Author           Date          Defect#    Change ID   Change Description
 *--------------------------------------------------------------------------------------------
-* Fabiane Kirsten  24/04/2023    N/A        001         Table FAH_RIL_DISCONTIN_ITEMS_HIST Creation
+* Fabiane Kirsten  01/06/2023    N/A        001         Table FAH_DISCONTINUED_ITEMS_GTT 
 *********************************************************************************************/
-begin
-  execute immediate 'drop table FAH_DISCONTINUED_ITEMS_GTT';
-exception
-  when others then
-    dbms_output.put_line(sqlerrm);
-end;
+-- Alter table
+alter table FAH_DISCONTINUED_ITEMS_GTT drop column no_movement_period
+/
+--
+alter table FAH_DISCONTINUED_ITEMS_GTT add no_movement_period  number
 /
 
--- Create table
-create global temporary table FAH_DISCONTINUED_ITEMS_GTT
-(
-  item VARCHAR2(25) not null,
-  loc  NUMBER(10) not null,
-  no_movement_period  number
-)
-on commit delete rows;
--- Grant/Revoke object privileges 
-grant select on FAH_DISCONTINUED_ITEMS_GTT to "ALFONSO.BUENROSTRO";
-grant select on FAH_DISCONTINUED_ITEMS_GTT to ARAMIREZ;
-grant select on FAH_DISCONTINUED_ITEMS_GTT to "ITZA.LOPEZ";
-grant select on FAH_DISCONTINUED_ITEMS_GTT to "MARTIN.GARCIA";
-grant select, insert, update, delete on FAH_DISCONTINUED_ITEMS_GTT to RMS;
-grant select on FAH_DISCONTINUED_ITEMS_GTT to RMS_CONSULTA;
-grant select on FAH_DISCONTINUED_ITEMS_GTT to "VALTRE_BERNARDO.SOTA";
